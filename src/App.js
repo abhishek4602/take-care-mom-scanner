b20cb128755364a3fbfb22443e1cb4f77f39087c
Deepali Kasture <kasture.d@northeastern.edu>
2022-02-28 02:20:37 -0500

    Adding App changes for uploading the image.

--- src/App.js
+++ src/App.js
@@ -7,28 +7,44 @@ import sampleImage from './assets/images.jpeg';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import Upload from './Upload/Upload.js';
+import { collection, getDocs } from "firebase/firestore"; 
+ import { getFirestore } from "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCKg-mGLcGthhMx4czEMds_k8GOXLSF12I",
-  authDomain: "takecaremom-60f9b.firebaseapp.com",
-  projectId: "takecaremom-60f9b",
-  storageBucket: "takecaremom-60f9b.appspot.com",
-  messagingSenderId: "259931007890",
-  appId: "1:259931007890:web:11d252d2e932c653daef4b",
-  measurementId: "G-WEYE16SP51"
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCKg-mGLcGthhMx4czEMds_k8GOXLSF12I",
+//   authDomain: "takecaremom-60f9b.firebaseapp.com",
+//   projectId: "takecaremom-60f9b",
+//   storageBucket: "takecaremom-60f9b.appspot.com",
+//   messagingSenderId: "259931007890",
+//   appId: "1:259931007890:web:11d252d2e932c653daef4b",
+//   measurementId: "G-WEYE16SP51"
+// };
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 
+//create new firebase project
+// validate if safety number present.
+
+
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js"></script>
 
 function App() {
-  useEffect(() => {
+ var x=1;
+  const db = getFirestore();
+  const [data1 , setData] = useState([]);
+//   useEffect(async() => {
   
-  });
+//         const querySnapshot = await getDocs(collection(db, "list"));
+// querySnapshot.forEach((doc) => {
+//   var data=`${doc.id} => ${doc.data()}`
+//   console.log(`${doc.id} => ${doc.data()}`);
+//   setData(data);
+// });
+//   },[x]);
   return (
     // <p spellCheck="true" >{e}</p>
-<Upload></Upload>
+<Upload data={data1}></Upload>
 
   );
 }
--- src/Upload/Upload.js
+++ src/Upload/Upload.js
@@ -1,47 +1,194 @@
    
 import React, { Component } from 'react';
- import { useState } from 'react';
+ import { useState,useEffect } from 'react';
 import Element from '../Upload/Element.js';
  import { createWorker } from 'tesseract.js';
  import Tesseract from 'tesseract.js';
+ import { collection, getDocs } from "firebase/firestore"; 
+ import { getFirestore } from "firebase/firestore"
+import { async } from '@firebase/util';
+import img1 from '../assets/1.png';
+import img2 from '../assets/2.png';
+import img3 from '../assets/3.png';
+import img4 from '../assets/4.png';
+import img5 from '../assets/5.png';
+import img6 from '../assets/6.png';
+import img7 from '../assets/7.png';
+import img8 from '../assets/8.png';
+import img9 from '../assets/9.png';
+import img10 from '../assets/10.png';
 
-export default function Upload() {
+
+export default function Upload(props) {
 
     const [selectedFile, setSelectedFile] = useState();
 	const [isFilePicked, setIsFilePicked] = useState(false);
+  const [safetyIndex, setSafetyIndex] = useState([]);
+    const [data, setData] = useState([]);
+    var y=[1,2,3];
+    const menuElements=safetyIndex
+    .map((c,i)=>   
   
+    <Element e={c} ></Element>); 
+
+  //var safetyIndex=[];
+  
+  var s=[];
+  var x=1;
     const [e,setE]=useState([]);
+    const db = getFirestore();
 	const changeHandler = (event) => {
 		//setSelectedFile(event.target.files[0]);
         setSelectedFile(URL.createObjectURL(event.target.files[0]));
 		setIsFilePicked(true);
 	};
+    useEffect(async() => {
+  var temp=[];
+        const querySnapshot = await getDocs(collection(db, "list"));
+querySnapshot.forEach((doc) => {
+  //var data=`${doc.id} => ${doc.data()}`
+  var data=doc.data();
+  console.log(`${doc.id} => ${doc.data()}`);
+  temp.push(data);
+});
+setData(temp);
+  },[x]);
+  const[finalscore,setFinalScore] =useState(y);
 
+const handleAvg= (array) =>{
+    let temp= array.reduce((a, b) => a + b) / array.length;
+    setFinalScore(temp);
+    console.log('finalscore '+temp)
+}
+var m=[];
 	const handleSubmission = () => {
+      //  console.log('data'+data);
         Tesseract.recognize(
             selectedFile,
             'eng',
             { logger: m => console.log(m) }
           ).then(({ data: { text } }) => {
-            setE(text.split(','));
+     m  =    text.split(',');
+     //console.log(m);
+    // console.log('data1'+data[0].id);
+       m.forEach(element=> {
+        data.forEach(d=>{
+        //  console.log(((d.name).toLowerCase()).trim()+' '+((element).toLowerCase()).trim());
+            if(((d.name).toLowerCase()).trim()===((element).toLowerCase()).trim())
+            {
+              s.push(d);
+              //safetyIndex.push(parseInt(d.safetyIndex));
+
+             
+              console.log('match found');
+            }
+        })
+
+       
+    })
+    console.log(s);
+    setSafetyIndex(s);
             
             console.log('data: ' + text);
-          })
+            
+            console.log('safetyIndex'+safetyIndex)
+         // check   handleAvg(safetyIndex)
+          }).then(
+            console.log('safetyIndex'+safetyIndex)
+        // handleAvg(safetyIndex)
+          )
+         // .then(handleAvg(safetyIndex))
+          //)
 	};
 
+    const getData= async()=>{
+        const querySnapshot = await getDocs(collection(db, "list"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
+    }
+    function getComponent(count){
+
+        console.log(count);
+       // console.log(selectedFile);
+        switch(Math.round(count)){
+          case 1: 
+            return <div><img src={img1} alt="Image"/><br></br></div>
+          case 2: 
+            return <div><img style={{width:"200px",height:"160px"}}  src={img2} alt="Image2"/><br/></div>;
+          case 3: 
+            return <div><img style={{width:"200px",height:"160px"}}    src={img3} alt="Image3"/><br/></div>;
+          case 4: 
+            return <div><img style={{width:"200px",height:"160px"}}    src={img4} alt="Image4"/><br/></div>;
+          case 5: 
+            return <div><img style={{width:"200px",height:"160px"}}    src={img5} alt="Image5"/><br/></div>;
+          case 6: 
+            return <div><img style={{width:"200px",height:"160px"}}     src={img6} alt="Image6"/><br/></div>;
+          case 7: 
+            return <div><img style={{width:"200px",height:"160px"}}   src={img7} alt="Image7"/><br/></div>;
+          case 8: 
+            return <div><img style={{width:"200px",height:"160px"}}     src={img8} alt="Image8"/><br/></div>;
+          case 9: 
+            return <div><img style={{width:"200px",height:"160px"}}   src={img9} alt="Image9"/><br/></div>;
+          case 10: 
+            return <div><img  style={{width:"200px",height:"160px"}}      src={img10} alt="Image10"/><br/></div>;
+           
+
+          // .. etcdefault: 
+            return <img src="" alt="Image default"/>;
+        }
+      }
+     
+
+    
 	return(
    <div>
-			<input type="file" name="file" onChange={changeHandler} />
-			<div>
-				<button onClick={handleSubmission}>Submit</button>
+       <div style={{display:"flex",flexDirection:"column",margin:"auto"}}>
+       
+       <div 
+            style={{display:"flex",flexDirection:"row",margin:"auto"}}>
+                
+	    <div style={{flex:1}}>
+        <input type="file" name="file"  onChange={changeHandler} />
+				
 			</div>
-            <div>
+            <div  style={{flex:1}}>
                 <img src={selectedFile} alt="cannot display" ></img>
             </div>
+
+             
+            
+            </div>
+            <button style={{width:"100px",margin:"auto"}}onClick={handleSubmission}>Submit</button>
+            
+            {/* <div style={{margin:"auto"}}>
+            {getComponent(finalscore)}
+
+                </div> */}
+                
+       </div>
+		<div>
+       {/* {safetyIndex.map((value,index)=>()} */}
+       {/* {finalscore.map(e => {
+        <li>{e}</li>
+        // <Element key={e} e={e}>{e}</Element>
+      })} */}
+      {/* {
+        safetyIndex.forEach(e=>{
+          console.log("value"+e);
+          getComponent(e);
+        })
+      } */}
+      {menuElements}
+      </div>   
+       
+          
+{/*          
             {e.map(e => (
         <Element key={e} e={e}>{e}</Element>
-      ))}
-           {/* <li spellCheck="true" >{e}</li> */}
-		</div>
+      ))} */}
+          
+		
+        </div>
 	)
 }
